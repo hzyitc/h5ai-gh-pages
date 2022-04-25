17659247fa4401a7df11517058487ed54924a8dd
hzy <h1260071766@126.com>
2022-04-25 22:40:17 +0800

    Migrate to ES modules

--- ghu.js
+++ ghu.js
@@ -19,7 +19,13 @@ const WEBPACK_CFG = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env']
+                        presets: [
+                            ['@babel/preset-env', {
+                                targets: {
+                                    esmodules: true
+                                }
+                            }]
+                        ]
                     }
                 }
             },
